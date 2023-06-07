00b94abebf7564434e16b2a3b1f77c1e2b34f4ed
AHX <vladis9484@gmail.com>
2023-06-07 21:21:49 +0300

    Added Car and News model

---
+++ Models/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace DriftNews.Models
+{
+    public class Car
+    {
+        public string CarBrand { get; set; }
+        public string Engine { get; set; }
+    }
+}
---
+++ Models/News.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DriftNews.Models
+{
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+        //[Required]
+        //public string Description { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public string ImgUrl { get; set; }
+
+    }
+}
