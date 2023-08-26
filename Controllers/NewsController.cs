aebe41723a0ee4bb4f3f695cedf1e576c74e662f
AHX <vladis9484@gmail.com>
2023-08-26 16:45:38 +0300

    Added News Page. Added Pagination for News Page

--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DriftNews.Data.Repository;
+using DriftNews.Models;
+using DriftNews.Models.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace DriftNews.Controllers
 {
     public class NewsController : Controller
     {
-        public IActionResult Index()
+        private readonly Repository _repository;
+        public NewsController(Repository repository)
         {
-            return View();
+            _repository = repository;
+        }
+        public IActionResult Index( int pg = 1)
+        {
+            List<NewsFD> news = _repository.GetNewsFD();
+            const int pageSize = 5;
+            if(pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = news.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = news.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
         }
     }
 }
