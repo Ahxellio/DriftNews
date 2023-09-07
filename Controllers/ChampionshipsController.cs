ea8e31f9908f0c6a9e39941eb9436b1f99576da4
AHX <vladis9484@gmail.com>
2023-09-07 20:58:37 +0300

    Added Championships Page For RDS & FD Updated NewsRDS Model

--- Controllers/ChampionshipsController.cs
+++ Controllers/ChampionshipsController.cs
@@ -1,24 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DriftNews.Models.Infrastructure;
+using DriftNews.Models;
+using Microsoft.AspNetCore.Mvc;
+using DriftNews.Data.Repository;
 
 namespace DriftNews.Controllers
 {
     public class ChampionshipsController : Controller
     {
+        private readonly Repository _repository;
+        public ChampionshipsController(Repository repository)
+        {
+            _repository = repository;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult RDS()
+        public IActionResult RDS(int pg = 1)
         {
-            return View();
+            List<NewsRDS> news = _repository.GetNewsRDS();
+            const int pageSize = 5;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = news.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = news.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
         }
         public IActionResult DMEC()
         {
             return View();
         }
-        public IActionResult FD()
+        public IActionResult FD(int pg = 1)
         {
-            return View();
+            List<NewsFD> news = _repository.GetNewsFD();
+            const int pageSize = 5;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = news.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recSkip = (pg - 1) * pageSize;
+            var data = news.Skip(recSkip).Take(pager.PageSize).ToList();
+            this.ViewBag.Pager = pager;
+            return View(data);
         }
     }
 }
